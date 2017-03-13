649c0d471f08260edf0958c5f97ef0a480b825c5
Gamelife <1185694600@qq.com>
2017-03-13 15:24:19 +0800

    pyton3转换搜狗拼音词库为txt文件

--- README.md
+++ README.md
@@ -1,23 +1,26 @@
-将搜狗(sogou)的细胞词库转换为mmseg的词库
+### 将搜狗(sogou)的细胞词库转换为txt文件
+----------------------------------------
 
-功能：
+环境：python3.5，python2请看：[https://github.com/archerhu/scel2mmseg](https://github.com/archerhu/scel2mmseg)
 
- - scel2mmseg.py: 将.scel文件转换为mmseg格式的.txt文件
- 
- 使用方法： python scel2mmseg.py a.scel a.txt
+使用方式如下：
 
- 批量转换方法：python scel2mmseg.py scel文件目录 a.txt
- 
- 说明：新增加的所有词的词频都为1，对于格式的解释如下：[摘自 http://jjw.in/server/226 ]
-> 每条记录分两行。其中，第一行为词项，其格式为：[词条]\t[词频率]。需要注意的是，对于单个字后面跟这个字作单字成词的频率，这个频率需要在大量的预先切分好的语料库中进行统计，用户增加或删除词时，一般不需要修改这个数值；对于非单字词，词频率处必须为1。第二行为占位项，是由于LibMMSeg库的代码是从Coreseek其他的分词算法库（N-gram模型）中改造而来的，在原来的应用中，第二行为该词在各种词性下的分布频率。LibMMSeg的用户只需要简单的在第二行处填”x:1″即可
+1.  作为工具使用
+    ```
+    python3 scel2txt.py ./ ./dict.txt
+    ```
+    第一个参数：`./` 表明scel文件的目录，也可以指定单个文件：`./亲戚称呼.scel`
+    第二个参数(可选)，默认为：`./dict.txt`
 
- - mergedict.py: 将mmseg的多个.txt文件合并为一个.txt
- 
- 使用方法： python mergedict.py unigram.txt b.txt c.txt new.txt
- 
- 说明： .txt可以使mmseg格式的，也可以是每行一个词的格式（这样词频默认为1）
- 
- 注意：因为merge的时候会判重，一个词在前面出现过，就不会追加到新产生的文件中,所以要将unigram.txt放到最前面
+2.  整合在其他程序中
+    ```python
+        for word in transform("./"):
+            print(word)
+    ```
+    或者
+    ```python
+       transform_and_save("./", "./dict.txt")
+    ```
 
 
 
---
+++ scel2txt.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env /usr/local/bin/python3
+# encoding: utf-8
+
+"""
+用于将搜狗拼音词库scel文件转换为txt文件
+@version: 0.1
+@author: Gamelfie
+@contact: fudenglong1417@gmail.com
+@time: 2017/3/13 13:02
+"""
+
+from io import BufferedReader
+import glob
+import struct
+import os
+import sys
+
+
+def __read_utf16_str(open_file, offset=-1, read_len=2):
+    assert isinstance(open_file, BufferedReader)
+    if offset >= 0:
+        open_file.seek(offset)
+    return open_file.read(read_len).decode('UTF-16LE')
+
+
+def __read_unit16(open_file):
+    assert isinstance(open_file, BufferedReader)
+    return struct.unpack('<H', open_file.read(2))[0]
+
+
+def transform(source_file_or_dir):
+    """
+    用于将搜狗拼音词库转换为txt文件
+    :param source_file_or_dir: scel文件或者包含scel文件的目录
+    :return: 返回包含word的生成器 generator
+    """
+
+    files = []
+
+    if os.path.isdir(source_file_or_dir):
+        if not source_file_or_dir.endswith("/"):
+            source_file_or_dir += "/"
+        files += glob.glob(source_file_or_dir + "*.scel")
+    elif os.path.isfile(source_file_or_dir):
+        files.append(source_file_or_dir)
+    else:
+        raise FileNotFoundError("文件路径不存在")
+
+    for file in files:
+        file_size = os.path.getsize(file)
+        try:
+            with open(file, "rb") as f:
+
+                hz_offset = 0
+                mask = struct.unpack('128B', f.read(128))[4]
+                if mask == 0x44:
+                    hz_offset = 0x2628
+                elif mask == 0x45:
+                    hz_offset = 0x26c4
+
+                title = __read_utf16_str(f, 0x130, 0x338 - 0x130)
+                type_ = __read_utf16_str(f, 0x338, 0x540 - 0x338)
+                desc = __read_utf16_str(f, 0x540, 0xd40 - 0x540)
+                samples = __read_utf16_str(f, 0xd40, 0x1540 - 0xd40)
+                # print("Title：%s" % title, "Type：%s" % type_, "Desc：%s" % desc, "Samples：%s" % samples, sep="\n")
+                py_map = {}
+                f.seek(0x1540 + 4)
+                while True:
+                    py_code = __read_unit16(f)
+                    py_len = __read_unit16(f)
+                    py_str = __read_utf16_str(f, -1, py_len)
+
+                    if py_code not in py_map:
+                        py_map[py_code] = py_str
+
+                    if py_str == 'zuo':
+                        break
+
+                f.seek(hz_offset)
+                while f.tell() != file_size:
+                    word_count = struct.unpack('<H', f.read(2))[0]
+                    pinyin_count = int(struct.unpack('<H', f.read(2))[0] / 2)
+
+                    py_set = []
+                    for i in range(pinyin_count):
+                        py_id = __read_unit16(f)
+                        py_set.append(py_map[py_id])
+                    py_str = "'".join(py_set)
+
+                    for i in range(word_count):
+                        word_len = __read_unit16(f)
+                        word_str = __read_utf16_str(f, -1, word_len)
+                        f.read(12)
+                        yield word_str
+        except KeyError:
+            # 这里不知道为什么会爆这个错误
+            pass
+
+
+def transform_and_save(source_file_or_dir, target_file):
+    """
+    转换并且保存为文件
+    :param source_file_or_dir: scel文件或者包含scel文件的目录
+    :param target_file: 保存的目标文件
+    :return:
+    """
+    words = []
+    for word_ in transform(source_file_or_dir=source_file_or_dir):
+        words.append(word_)
+    with open(target_file, "w+", encoding="utf8") as f:
+        f.write("\n".join(words))
+
+
+def main():
+
+    if len(sys.argv) < 2:
+        raise Exception("缺少必要的参数")
+    source_file_or_dir = sys.argv[1]
+
+    target_file = "./dict.txt"
+    if len(sys.argv) == 3:
+        target_file = sys.argv[2]
+
+    transform_and_save(source_file_or_dir, target_file)
+
+if __name__ == "__main__":
+    main()
